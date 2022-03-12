f4f447a29957ba794e74c90e8210f4ae8632c02a
CheshireCat16 <john.cheshire@gmail.com>
2022-03-11 20:32:27 -0330

    Added ability to show comments

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -13,34 +13,93 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     let myRefreshControl = UIRefreshControl()
     var numPhotos = 20
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // Handle logout button
+    @IBAction func onLogoutButton(_ sender: Any) {
+        print("logging")
+        PFUser.logOut()
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewControl = main.instantiateViewController(withIdentifier: "LoginViewController")
+        
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else { return }
+        
+        delegate.window?.rootViewController = loginViewControl
+        
+
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return posts.count
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Get number of comments
+        let post = posts[section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
+        
+        return comments.count + 1
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        let post = posts[indexPath.section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
+        
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+            
+            
+            let user = post["author"] as! PFUser
+            cell.usernameLabel.text = user.username
+            
+            cell.commentLabel.text = post["caption"] as? String
+            
+            let imageFile = post["image"] as! PFFileObject
+            let urlString = imageFile.url!
+            let url = URL(string: urlString)!
+            
+            cell.photoView.af.setImage(withURL: url)
+            
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell") as! CommentCell
+            
+            let comment = comments[indexPath.row - 1]
+            cell.commentLabel.text = comment["text"] as? String
+            
+            let user = comment["author"] as! PFUser
+            cell.commenterLabel.text = user.username
+            
+            return cell
+        }
+
+        
+        
+    }
+    
+    // Calls when a user taps something
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let post = posts[indexPath.row]
         
-        let user = post["author"] as! PFUser
-        cell.usernameLabel.text = user.username
+        let comment = PFObject(className: "Comments")
+        comment["text"] = "This is a random comment"
+        comment["post"] = post
+        comment["author"] = PFUser.current()!
         
-        cell.commentLabel.text = post["caption"] as? String
+        post.add(comment, forKey: "comments")
         
-        let imageFile = post["image"] as! PFFileObject
-        let urlString = imageFile.url!
-        let url = URL(string: urlString)!
-        
-        cell.photoView.af.setImage(withURL: url)
-        
-        return cell
-        
+        post.saveInBackground { succes, error in
+            if succes {
+                print("Comment saved")
+            } else {
+                print("Error saving comment")
+            }
+        }
         
     }
     
     @objc func refreshPhotos() {
         let query = PFQuery(className: "Posts")
         
-        query.includeKey("author")
+        query.includeKeys(["author", "comments", "comments.author"])
         
         self.numPhotos = 20
         query.limit = self.numPhotos
